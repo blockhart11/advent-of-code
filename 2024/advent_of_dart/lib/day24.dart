bcec1be03e8c11e5207bfefb365c739c37008c47
blockhart11 <51723196+blockhart11@users.noreply.github.com>
2024-12-29 11:29:23 -0700

    Day 24 Start
    Just want to move this to the other computer

--- 2024/advent_of_dart/lib/day24.dart
+++ 2024/advent_of_dart/lib/day24.dart
@@ -2,7 +2,14 @@ mixin Day24 {
   int day24a(List<String> lines) {
     int result = 0;
 
-    // do the thing
+    int i = 0;
+    for( ; lines[i].isNotEmpty; i++) {
+      // load wires
+    }
+    i++;
+    for( ; i < lines.length; i++) {
+      // load gates
+    }
 
     return result;
   }
